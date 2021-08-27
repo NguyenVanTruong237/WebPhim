b12ad2dd6caa47367e836bbb8d2da1e5cf211090
Truong10Ngon <toiiuvn013@gmail.com>
2021-08-27 19:09:24 +0700

    [Authorize]

--- WebPhim/App_Start/FilterConfig.cs
+++ WebPhim/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace WebPhim
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
