4d16ce843598ab03ce8a936c416d6bfb6fe5ed8b
Truong10Ngon <toiiuvn013@gmail.com>
2021-08-28 14:09:16 +0700

    Seeding Users and Roles

--- WebPhim/Controllers/AccountController.cs
+++ WebPhim/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using WebPhim.Models;
@@ -155,6 +156,12 @@ namespace WebPhim.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //TEMP Code
+                    //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    //var roleManage = new RoleManager<IdentityRole>(roleStore);
+                    //await roleManage.CreateAsync(new IdentityRole("Admin"));
+                    //await UserManager.AddToRoleAsync(user.Id, "Admin");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
